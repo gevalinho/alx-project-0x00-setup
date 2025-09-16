4428a2e10c1994c2a8b6ab03cc6f243a60fea20e
gevalinho <gevalinho1@gmail.com>
2025-09-16 13:08:14 +0100

    Advanced Task

---
+++ alx-project-0x00/interfaces/index.ts
@@ -0,0 +1,12 @@
+export interface PillProps {
+  title: string
+}
+
+// interfaces/index.ts
+
+export interface ButtonProps {
+  title: string;
+  size?: "small" | "medium" | "large";
+  shape?: "rounded-sm" | "rounded-md" | "rounded-full";
+  onClick?: () => void;
+}
--- alx-project-0x00/pages/landing.tsx
+++ alx-project-0x00/pages/landing.tsx
@@ -1,12 +1,39 @@
 import React from "react"
 import Card from "@/components/Card"
+import Button from "@/components/Button";
 
 const Landing: React.FC =  () => {
   return (
-    <div>
+   
+    <main className="flex flex-col h-screen justify-center items-center space-y-4">
+      <h1 className="text-3xl font-bold mb-6">Landing Page Buttons</h1>
+
+       <div>
       <h1 className=" text-xl font-extralight">Landing Page</h1>
       <Card />
     </div>
+
+      {/* Small buttons */}
+      <div className="flex gap-4">
+        <Button title="Small Sm" size="small" shape="rounded-sm" />
+        <Button title="Small Md" size="small" shape="rounded-md" />
+        <Button title="Small Full" size="small" shape="rounded-full" />
+      </div>
+
+      {/* Medium buttons */}
+      <div className="flex gap-4">
+        <Button title="Medium Sm" size="medium" shape="rounded-sm" />
+        <Button title="Medium Md" size="medium" shape="rounded-md" />
+        <Button title="Medium Full" size="medium" shape="rounded-full" />
+      </div>
+
+      {/* Large buttons */}
+      <div className="flex gap-4">
+        <Button title="Large Sm" size="large" shape="rounded-sm" />
+        <Button title="Large Md" size="large" shape="rounded-md" />
+        <Button title="Large Full" size="large" shape="rounded-full" />
+      </div>
+    </main>
   )
 }
 export default Landing
